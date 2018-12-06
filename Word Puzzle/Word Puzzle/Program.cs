f82456a54b8ae57b43786641bbe530f663e7f4e3
mbugrates4 <45660629+mbugrates4@users.noreply.github.com>
2018-12-06 16:58:50 +0300

    Add files via upload

---
+++ Word Puzzle/Word Puzzle/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////VARIABLES START
+            bool application = true;
+            ////VARIABLES END
+            
+
+
+
+            /////INPUT FILES START
+            
+            /////INPUT FILES END
+            ///
+
+
+
+            /////PREPARE VARIABLES START
+
+            /////PREPARE VARIABLES END
+
+
+            
+            //////APPLICATION LOOP START
+            while (application)
+            {
+                bool game = true;
+
+                /////START SCREEN START
+
+                //////START SCREEN END
+                ///
+
+
+                //////TUTORIAL START
+
+                /////TUTORIAL END
+
+
+
+                ///////GAME LOOP START
+                while (game)
+                {
+                    bool slotNotFound = true;
+
+
+                    /////PRINT PUZZLE START
+
+                    /////PRINT PUZZLE END
+
+
+
+                    
+                    
+                    
+                    ////////////////////////////////////SCAN SLOTS START
+
+
+                    //specify loop START
+                    while (slotNotFound)
+                    {
+
+                    }
+                    //specify loop end
+                    
+                    
+                    //slot anaysis loop start
+                    while (slotNotFound)
+                    {
+
+                    }
+                    // slot alaysis loop end
+
+
+                    //step by step loop start
+                    while (slotNotFound)
+                    {
+
+                    }
+                    //step by step loop end
+                    
+                    ///////////////////////////////////SCAN SLOTS END
+
+
+
+                    
+                
+                
+                
+                    /////FIND WORD START
+
+
+                    //////FIND WORD END
+
+
+
+
+                    ///////FIT WORD START
+
+                    ///////FIT WORD END
+
+
+
+                    ///////CONTROL PUZZLE FISINHED START
+
+                    ///////CONTROL PUZZLE FINISHED END
+                }
+                ////////GAME LOOP END
+
+
+
+                //////CREATE SOLUTION ARRAY START
+
+                //TO TEXT
+
+                ////////CREATE SOLUTION ARRAY END
+
+
+
+                //////END SCREEN START
+
+                //////END SCREEN END
+
+
+            }
+            ///////APPLICATION LOOP END
+
+
+
+        }
+    }
+}
